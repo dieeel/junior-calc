f8987b7837cb036e0bdef461f5588d0230d69285
dieeel <by.gonece@gmail.com>
2022-12-30 17:55:12 +0900

    create title

--- junior-calc/src/components/VCalcGame.vue
+++ junior-calc/src/components/VCalcGame.vue
@@ -1,5 +1,21 @@
 <template>
-    <div>
+    <div class="title">
         <h1>けいさん</h1>
+        <div class="marker"></div>
     </div>
 </template>
+
+<style>
+.title{
+    position: relative;
+}
+
+.marker{
+    width: 100%;
+    height: 35%;
+    background-color: #a2a2a270;
+    position: absolute;
+    bottom: 5%;
+    z-index: -1;
+}
+</style>
