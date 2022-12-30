87d01ea29ffb9b2246646299732425c2fd212a32
dieeel <by.gonece@gmail.com>
2022-12-30 22:46:34 +0900

    game template and style

--- junior-calc/src/components/VCalcGame.vue
+++ junior-calc/src/components/VCalcGame.vue
@@ -1,13 +1,40 @@
 <template>
-    <div class="title">
-        <h1>けいさん</h1>
-        <div class="marker"></div>
+    <div class="container">
+        <div class="title">
+            <h1>けいさん</h1>
+            <div class="marker"></div>
+        </div>
+    <button class="startButton mb-20">スタート</button>
+    <div class="quession mb-20">aaa</div>
+    <div class="clear">がんばったね</div>
+    <div class="typeFormWrapper mb-20">
+        <input type="text" class="typeForm">
+    </div>
+
+    <div class="gaugeWrapper mb-20">
+        <div class="gauge"></div>
+    </div>
+
+    <div>1/5</div>
+
+
     </div>
 </template>
 
 <style>
+.container{
+    width: 400px;
+    margin: 0 auto;
+    text-align: center;
+}
+
+.mb-20{
+    margin-bottom: 20px;
+}
+
 .title{
     position: relative;
+    font-size: 48px;
 }
 
 .marker{
@@ -18,4 +45,42 @@
     bottom: 5%;
     z-index: -1;
 }
+
+.startButton{
+    background-color: #333;
+    color: #fff;
+    padding: 4px 60px;
+    border: none;
+    outline: none;
+    border-radius: 8px;
+    cursor: pointer;
+}
+
+.quession{
+    color: gray;
+}
+
+.clear{
+    color: #03a9f4;
+}
+
+.typeForm{
+    text-align: center;
+    outline: none;
+    border: none;
+}
+
+.typeFormWrapper{
+    border-bottom: 1px solid gray
+}
+
+.gauge{
+    height: 12px;
+}
+
+.gaugeWrapper{
+    border: 1px solid;
+    height: 12px;
+}
+
 </style>
